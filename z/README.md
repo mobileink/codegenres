990d579f279f7e53c9770028d22a6e925f994bf4
Gregg Reynolds <dev@mobileink.com>
2014-05-11 10:00:10 -0500

    move z stuff to proofiness repo

--- z/README.md
+++
@@ -1,7 +0,0 @@
-# Z
-
-This directory contains the lexis and syntax definitions for the Z
-notation taken from "ISO/IEC 13568 Z formal specification notation".
-
-Maybe someday it will also contain a parser that would allow us to
-embed Z notation in Clojure docstrings and comments.
--- z/parser/project.clj
+++
@@ -1,7 +0,0 @@
-(defproject zparser "0.1.0-SNAPSHOT"
-  :description "Parser for Z formal specification notation"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [instaparse "1.3.2"]])
